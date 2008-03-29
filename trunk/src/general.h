ff532d648e7fa56df11df3b1a6d0fe9b44dfcc0e
Dimitri.Michaux <Dimitri.Michaux@811700b5-6b49-0410-a1a1-3917990f4dc1>
2008-03-29 18:29:40 +0000

    Update of changelog +start of version 0.1.3

--- trunk/src/general.h
+++ trunk/src/general.h
@@ -5,7 +5,7 @@
 #define DEVID_PATH "/dev/disk/by-id/"
 #define STATS_PATH "/proc/diskstats"
 #define CHAR_BUF   256
-#define VERSION    "0.1.2"
+#define VERSION    "0.1.3"
 
 /*  These are commands that are send by Spindown and
     are accepted by Disk. They are unsigned chars     */
