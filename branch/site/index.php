f949eeceeecb5224d142be568aaf33ea90473ea1
Dimitri.Michaux <Dimitri.Michaux@811700b5-6b49-0410-a1a1-3917990f4dc1>
2008-04-03 20:39:58 +0000

    Here I am going to put my site under version control.

---
+++ branch/site/index.php
@@ -0,0 +1,16 @@
+<?php $title = "";
+  include("header.html"); ?>
+
+<h1>Projects</h1>
+
+<ul>
+  <li><a href="spindown.php">Spindown</a></li>
+  <li><a href="channelchanger.php">Channelchanger</a></li>
+</ul>
+
+<h1>Forum</h1>
+
+<p><a href="forum/">There is a forum</a> for all your questions and other needs.</p>
+
+<?php $updated = "20/03/2008";
+  include("footer.html"); ?>
